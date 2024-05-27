c3ba299112c1755afc2a122ca6c882b5df00e726
Eddie Andres Castro <eacstro@gmail.com>
2024-05-27 05:34:37 -0500

    Update

--- .godot/mono/temp/obj/Debug/Budget App.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Budget App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Budget App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddc90ad2e849fdbb9693c6efd52348ad06a25276")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+053fb9cde263e87090226a868357d9c3aa6c26b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Budget App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Budget App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AddTransactionWindow.cs
+++ AddTransactionWindow.cs
@@ -27,7 +27,7 @@ public partial class AddTransactionWindow : Panel
 		EmitSignal(SignalName.AddTransaction, 
 			GetNode<LineEdit>("VBoxContainer/Name/LineEdit").Text,
 			GetNode<LineEdit>("VBoxContainer/Date/LineEdit").Text,
-			int.Parse(GetNode<LineEdit>("VBoxContainer/Amount/LineEdit").Text),
+			float.Parse(GetNode<LineEdit>("VBoxContainer/Amount/LineEdit").Text),
 			GetNode<OptionButton>("VBoxContainer/Type/Type").Selected,
 			GetNode<CheckButton>("VBoxContainer/Income/CheckButton").ButtonPressed
 		);
